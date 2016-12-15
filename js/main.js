5d81765acfada2e343c2d14de5efda17f85dd928
wilsonjr16 <wilsonjr.dev@gmail.com>
2016-12-15 20:23:29 -0300

    site de divulgação

---
+++ js/main.js
@@ -0,0 +1,9 @@
+jQuery(document).ready(function($) {
+    $("#slide").owlCarousel({
+        navigation : false, 
+        slideSpeed : 300,
+        paginationSpeed : 400,
+        pagination : true,
+        items : 3
+    });
+});
